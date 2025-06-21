72e79c8d9785fec0143463dead711e3f4114c09d
ArjunTate1 <tatearjun2006@gmail.com>
2025-06-21 16:43:12 +0530

    Added function

---
+++ Dowhile.java
@@ -0,0 +1,11 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Arjun");
+            i++;
+        } while (i <= 5);
+    }
+
+}
---
+++ Function.java
@@ -0,0 +1,13 @@
+public class Function {
+
+
+    // Function to multiply two numbers
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(2, 8);
+        System.out.println("Multiplication is: " + result);
+    }
+}
